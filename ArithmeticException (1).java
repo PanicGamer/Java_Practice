b5d4fe150f54715bec8b2e9d0f67bbf38e10082e
Chandresh Bhuvana Sai <103114406+kchandresh@users.noreply.github.com>
2022-05-02 17:52:40 +0530

    Add files via upload

---
+++ ArithmeticException (1).java
@@ -0,0 +1,26 @@
+/* 1. Write a program which will illustrate ArthmeticException. Create a parameterized method which can take two values and print division of two numbers. 
+Please write your comments regarding ArithmeticException such as "When ArithmeticException will occur in this program?"
+*/
+import java.util.Scanner;
+class ArithmeticException
+{
+  public static void main(String args[])
+  {
+ //using scanner we can read i/p from the user storeed in a and b
+    Scanner sc=new Scanner(System.in);
+    System.out.println("enter the first number");
+    int a=sc.nextInt();
+    System.out.println("enter the second number");
+    int b=sc.nextInt();
+    
+     int  e=(a*b)/0;
+    
+   
+      
+        System.out.println(e);
+      
+    System.out.println("Exception handled");
+  }
+}
+
+  //by using try and get method we can exception handled
---
+++ Private.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+  class Student
+    {
+      private int rollno;
+      private String name;
+    void setRollno(int rollno)
+      {
+        this.rollno=rollno;
+      }
+      int getRollno()
+      {
+        return this.rollno;
+      }
+      void setName(String name)
+      {
+        this.name=name;
+      }
+      String getName()
+      {
+        return this.name;
+      }
+    }
+
+
+class Private
+  {
+    public static void main(String args[])
+    {
+      Scanner sc=new Scanner(System.in);
+      Student st=new Student();
+      System.out.println("enter the roll no");
+      st.setRollno(sc.nextInt());
+      System.out.println("enter the name");
+      st.setName(sc.next());
+      System.out.println(st.getRollno()+st.getName());
+    }
+  }
